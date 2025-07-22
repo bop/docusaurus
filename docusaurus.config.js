ac3e44213d5df1ad5bbc712d358088c903ccc601
Baptiste <baptiste.ossipow@gmail.com>
2025-07-22 22:17:36 +0200

    version 1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,146 +10,134 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'KnowingSite',
-  tagline: 'Dinosaurs are dead yet',
-  favicon: 'img/favicon.ico',
+    title: 'KnowingSite',
+    tagline: 'Dinosaurs are dead yet',
+    favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
+    // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+    future: {
+	v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    },
 
-  // Set the production url of your site here
-  url: 'https://plastish.surge.sh/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: 'https://plastish.surge.sh/',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'bop', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'bop', // Usually your GitHub org/user name.
+    projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'fr',
-    locales: ['fr'],
-  },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+	defaultLocale: 'fr',
+	locales: ['fr'],
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
+    presets: [
+	[
+	    'classic',
+	    /** @type {import('@docusaurus/preset-classic').Options} */
+	    ({
+		docs: {
+		    sidebarPath: './sidebars.js',
+		    // Please change this to your repo.
+		    // Remove this to remove the "edit this page" links.
+		    editUrl:
+		    'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+		},
+		blog: {
+		    showReadingTime: true,
+		    feedOptions: {
+			type: ['rss', 'atom'],
+			xslt: true,
+		    },
+		    // Please change this to your repo.
+		    // Remove this to remove the "edit this page" links.
+		    editUrl:
+		    'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+		    // Useful options to enforce blogging best practices
+		    onInlineTags: 'warn',
+		    onInlineAuthors: 'warn',
+		    onUntruncatedBlogPosts: 'warn',
+		},
+		theme: {
+		    customCss: './src/css/custom.css',
+		},
+	    }),
+	],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'KnowingSite',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/bop/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
+	// Replace with your project's social card
+	image: 'img/docusaurus-social-card.jpg',
+	navbar: {
+            title: 'KnowingSite',
+            logo: {
+		alt: 'My Site Logo',
+		src: 'img/logo.svg',
+            },
             items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
+		{
+		    type: 'docSidebar',
+		    sidebarId: 'tutorialSidebar',
+		    position: 'left',
+		    label: 'Tutorial',
+		},
+		{to: '/blog', label: 'Blog', position: 'left'},
+		{
+		    href: 'https://github.com/bop/docusaurus',
+		    label: 'GitHub',
+		    position: 'right',
+		},
             ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
+	},
+	footer: {
+            style: 'dark',
+            links: [
+		{
+		    title: 'Docs',
+		    items: [
+			{
+			    label: 'Tutorial',
+			    to: '/docs/intro',
+			},
+		    ],
+		},
+		{
+		    title: 'Community',
+	            items: [
+			{
+			    label: 'GitHub',
+			    href: 'https://github.com/bop/docusaurus',
+			},
+		    ],
+		},
+		{
+		    title: 'More',
+		    items: [
+			{
+			    label: 'Blog',
+			    to: '/blog',
+			},
+		    ],
+		},
             ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/bop/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} poetic.ch, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
+            copyright: `Copyright © ${new Date().getFullYear()} poetic.ch, Inc. Built with Docusaurus.`,
+	},
+	prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+	},
     }),
 };
 
